a8de5aea3c09bc976271815addb9f3da318be062
Dmitriy <dm.tsiganov@gmail.com>
2025-07-01 22:09:33 +0700

    01.07/2

--- HabitAtom/models.py
+++ HabitAtom/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from .validators import validate_habit_fields, validate_pleasant_habit, validate_performance
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 User = get_user_model()
 
 class Habit(models.Model):
+    name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
     place = models.CharField(max_length=255)
     time = models.TimeField()
     action = models.CharField(max_length=255)
-    is_pleasant = models.BooleanField(default=False, helptext="Приятная привычка?")
+    is_pleasant = models.BooleanField(default=False, help_text="Приятная привычка?")
+    last_performed = models.DateField(blank=True, null=True)
     related_habit = models.ForeignKey(
         'self', null=True, blank=True, on_delete=models.SET_NULL, related_name='linked_habits',
         help_text="Связанная полезная привычка"
@@ -17,6 +22,21 @@ class Habit(models.Model):
     reward = models.CharField(max_length=255, blank=True)
     duration = models.PositiveSmallIntegerField(help_text="Время на выполнение в минутах")
     is_public = models.BooleanField(default=False, help_text="Публичная привычка?")
+
+    def clean(self):
+        validate_habit_fields(self)
+        validate_pleasant_habit(self)
+        validate_performance(self)
+
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            if self.last_performed:
+                time_taken = (timezone.now() - self.last_performed).total_seconds()
+                if time_taken > 120:
+                    raise ValidationError("Время выполнения не должно превышать 120 секунд.")
+
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f'{self.action} в {self.place} в {self.time}'
